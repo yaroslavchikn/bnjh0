bbd6b26914b1fd8872666df44ec6f8f296cc6c86
Yarchik <yaroslav777007@gmail.com>
2026-09-06 12:00:42 +0500

    Create main.dart

---
+++ lib/main.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+
+import 'app.dart';
+
+void main() {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  SystemChrome.setPreferredOrientations(
+    [
+      DeviceOrientation.portraitUp,
+      DeviceOrientation.portraitDown,
+    ],
+  );
+
+  runApp(const MediaXApp());
+}
